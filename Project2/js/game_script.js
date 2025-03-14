a85e6a54c259ef059a542504d534211456109b96
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-03-14 15:01:01 -0700

    Update game_script.js

--- Project2/js/game_script.js
+++ Project2/js/game_script.js
@@ -131,6 +131,6 @@ document.getElementById('guess_button').addEventListener('click', async function
 
     // After 6th word, display game over in an alert
     if (num_guessed_words === 6) {
-        alert("Game Over! \n You've used all available six guesses without guessing the correct word. Reset the game to play again!");
+        alert(`Game Over! \n You've used all available six guesses without guessing the correct word, which was "${answer_to_use}". \n Reset the game to play again!`);
     }
 });
