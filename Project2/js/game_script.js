7f5c9530fdec830aac42371ebcf7b3774c1563af
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-03-14 02:09:51 -0700

    Update game_script.js

--- Project2/js/game_script.js
+++ Project2/js/game_script.js
@@ -1,52 +1,32 @@
-    // Create the board
-    // Get the board out by it's div id so that can populate it here
-    // const board = document.getElementById('board');
+// Create function to create all words (rows) of board at once --> one word = one row so create all 6 words (all 6 rows)
+create_rows = () => {
+    // Get board out by it's div id so that can populate it here
+    const board = document.getElementById("board");
 
-    // Create a function to create all words (rows) of board at once --> one word is one row so create all 6 words (all 6 rows)
-    // curr_row input param represents the row that we are creating (have 6 total rows so this is a num from 0 to 5)
-    // create_rows = () => {
-    //     // Loop through 6 times and create all 6 rows of the board using function just defined for creating one single row
-    //     for (let i=0; i<6; i++) {
-    //         // Store each row as a div
-    //         const row = document.createElement("div");
-
-    //         // Step (d) in hints of spec says to add CSS class to div to be able style each row so do so
-    //         row.className = "each_row";
-
-    //         // Loop through 5 times and create all 5 cells in each row
-    //         for (let j=0; j<5; j++) {
-    //             // Create div for each cell = (b) in hints of specs
-    //             const cell = document.createElement("div");
-
-    //             // Add 2 CSS classes to each cell: (1) style each cell + (2) identify position of each cell
-    //             // Position of each cell = given by row, col so put that as the position of each
-    //             cell.classList.add("each_cell", `position_${i}_${j}`);
-
-    //              // Actually add each cell into the row div
-    //             console.log(row);
-    //             row.appendChild(cell);
-    //         }
-            
-    //         // Once out of that loop, then are done with all of the cells in each row so add the now completed row to the board
-    //         board.appendChild(row);
-    //     }
-    // };
-
-initBoard = () => {
-    let board = document.getElementById("board");
-
+    // Loop through 6 times + create all 6 rows of board
     for (let i = 0; i < 6; i++) {
-        let row = document.createElement("div")
-        row.className = "each_row"
+        // Store each row as a div
+        const row = document.createElement("div");
 
+        // Step (d) in hints of spec says to add CSS class to div to be able style each row so do so
+        row.className = "each_row";
+
+        // Loop through 5 times and create all 5 cells in each row
         for (let j = 0; j < 5; j++) {
-            let cell = document.createElement("div")
-            cell.className = "each_cell"
-            row.appendChild(cell)
-        }
+            // Create div for each cell = (b) in hints of specs
+            const cell = document.createElement("div");
 
-        board.appendChild(row)
+            // Add style class to each cell
+            cell.className = "each_cell";
+
+            // Actually add each cell into the row div
+            row.appendChild(cell);
+        }
+        
+        // Once out of that loop, then are done with all of the cells in each row so add the now completed row to the board
+        board.appendChild(row);
     }
 }
 
-initBoard()
+// Now actually call the function so that the board is created on page
+create_rows();
