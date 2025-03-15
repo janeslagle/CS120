3377483d2d82fc7a2ef60ce58baafb29bac0ef04
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-03-14 19:55:30 -0700

    Update game_script.js

--- Project2/js/game_script.js
+++ Project2/js/game_script.js
@@ -217,7 +217,7 @@ play_game = async () => {
             }
 
             // If have made it here then means that the letter adding in cell is not in the answer word, so color the cell with not_in_word_letter class
-            else if (!answer_to_use.includes(word_letter.toUpperCase())) { {
+            else if (!answer_to_use.includes(word_letter.toUpperCase())) {
                 current_row[i].classList.add("not_in_word_letter");
             }
         });
