e9b154a8240c416fe4a6ccc6272dd7e99ff4499d
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-03-30 11:37:41 -0700

    Update db1.php

--- Assignment: PHP Practice/db1.php
+++ Assignment: PHP Practice/db1.php
@@ -32,6 +32,36 @@ $query_result = $conn->query($sql_query);
       h1 {
         margin-left: 10px;
       }
+
+      form {
+        margin-left: 35px;
+        margin-bottom: 20px;
+      }
+
+      label {
+    display: block;
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+
+  input[type="checkbox"] {
+    margin-right: 10px;
+  }
+
+  button {
+    background-color: #4CAF50; /* Green background */
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+  }
+
+  button:hover {
+    background-color: #45a049; /* Darker green when hovered */
+  }
     </style>
   </head>
 
