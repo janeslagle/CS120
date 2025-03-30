c689bca38bffa245cc670a7e2e2654bbeebe510b
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-03-30 11:33:18 -0700

    Update db1.php

--- Assignment: PHP Practice/db1.php
+++ Assignment: PHP Practice/db1.php
@@ -1,22 +1,23 @@
 <?php
 // Part 1: db1.php
-// First create connection to DB from past assignment 
-// Get everything out of DB that need to connect to it
-$servername = "localhost";
-$username="uzjxte4jj5gyg";
-$password="hyfirofzudwo";
-$dbname = "dbte2fhuna2wku";
+// First create connection to DB from past assignment
+// Get everything out of DB that need to connect to it: the user that have added to DB, everything, etc.!
+$server_name = "localhost";
+$user_name = "uzjxte4jj5gyg";
+$password = "hyfirofzudwo";
+$db_name = "dbte2fhuna2wku";
 
-// Now make the connection
-$conn = new mysqli($servername, "", "", $dbname);
+// Now make the connection to the DB
+$conn = new mysqli($server_name, $user_name, $password, $db_name);
 
-// Make sure can connect to the DB
+// Check if the connection to DB worked or not: if didn't, display it on page
 if ($conn->connect_error) {
   die("Failed to connect to database dbte2fhuna2wku (Song List): " . $conn->connect_error);
 }
 
-// Now get the genres out of the genre table -> have column genre_name where all the genres are
-$sql_query = "SELECT genre_name FROM genre"; 
+// If have gotten here: means that successfully connected to DB so get the genres out now
+// Genres are in genre table in genre_name column
+$sql_query = "SELECT genre_name FROM genre";
 $query_result = $conn->query($sql_query);
 ?>
 
@@ -24,28 +25,40 @@ $query_result = $conn->query($sql_query);
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width", initial-scale=1.0">
-    <title>Select Song Genres</title>    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song Genre Selection</title>
+
+    <style>
+      h1 {
+        margin-left: 10px;
+      }
+    </style>
   </head>
-<body>
-  <h1>Select Song Genres</h1>
-  <form action=db2.php" method="get">
-    <?php
-      // See if there are any genres fetched from the DB and then create form out of them
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-          echo '<label><input type="checkbox" name="genres[]" value="' . $row['genre_name']. '">' . $row['genre_name'] . '</label><br>';
+
+  <body>
+    <h1>Select Song Genres</h1>
+
+    <form action=_datab2__.php" method="get">
+      <?php
+      // First check if any genres were pulled out of DB before try to loop through them
+      if ($query_result->num_rows > 0) {
+        // Then means have genres to create form with
+        // Loop through all available genres have pulled out
+        while($row = $query_result->fetch_assoc()) {
+          // Have checkbox that has a label that is the song genre, put line between each checkbox in form
+          echo '<label><input type="checkbox" name="genres[]" value="' . $row['genre_name']. '">' .$row['genre_name'] . '</label><br>';
         }
       } else {
         echo "No genres found in database dbte2fhuna2wku (Song List)";
       }
-    ?>
-    <button type="submit">Submit></button>
-  </form>
-</body>
+      ?>
+
+      <button type="submit">Submit</button>
+    </form>
+  </body>
 </html>
 
 <?php
-// Close the connection to the DB now that are done
+// Close connection to DB now that are done
 $conn->close();
 ?>
