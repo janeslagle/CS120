8c79b7e2f961f2339218a675ff3077fc0cefea79
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-04-20 00:18:03 -0700

    Update cart.php

--- Project3/cart.php
+++ Project3/cart.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cart - Jane's Fav Books</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="stylesheets/cart.css">
+</head>
 
+<body>
+    <!-- Display the navigation bar -->    
+    <div class="nav_bar">
+        <div class="logo">
+            Jane's Fav Books
+            <img src="images/favicon.ico" alt="favicon">
+        </div>
+
+        <!-- Display the other items in the nav bar -->
+        <div class="nav_bar_items">
+            <a href="products.php">Products</a>
+            <a href="cart.php">Cart</a>
+            <a href="orders.php">Orders</a>
+        </div>
+    </div>
+</body>
+</html>
